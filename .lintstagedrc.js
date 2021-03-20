35b7e4e39e29ca3fdafe33eb0131a885bdba597f
Matt Farmer <matt.farmer@wizeline.com>
2021-03-20 14:31:07 -0700

    ci: fix ESLint pre-commit hook

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
   '*.{css,js,json,md,ts}': 'prettier --write',
-  '*.{js,ts}': 'eslint --fix',
+  '*.{js,ts}': 'eslint --ext .js,.ts --max-warnings=0 --fix',
   '**/*.ts?(x)': () => 'tsc -p tsconfig.json --noEmit',
 };
