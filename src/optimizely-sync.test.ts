63e9afe9ce8aee9fd7d3d2c4323fc16401e6886f
Matt Farmer <matt.farmer@wizeline.com>
2021-04-29 09:25:35 -0700

    test: add tests for optimizely-sync's `deleteFeatures` function

--- src/optimizely-sync.test.ts
+++ src/optimizely-sync.test.ts
@@ -119,6 +119,190 @@ describe('optimizely-sync', () => {
     it('is a function', () => {
       expect(typeof deleteFeatures).toBe('function');
     });
+
+    it('does nothing if there is nothing to delete', async () => {
+      const consoleLog = jest.spyOn(console, 'log').mockImplementation();
+
+      const dryRun = false;
+      const mockedOptimizelyClient = {
+        deleteFeature: jest.fn(),
+      };
+      const config: OptimizelySyncConfig = {
+        someEnv: {
+          someFeature: 10000,
+        },
+      };
+      const features: PartialFeature[] = [
+        {
+          key: 'someFeature',
+          environments: {
+            someEnv: {
+              id: 123,
+              is_primary: true,
+              rollout_rules: [
+                {
+                  audience_conditions: 'everyone',
+                  enabled: true,
+                  percentage_included: 10000,
+                },
+              ],
+            },
+          },
+        },
+      ];
+
+      await deleteFeatures(
+        dryRun,
+        // @ts-expect-error -- intentionally passing a partial OptimizelyConfig
+        mockedOptimizelyClient,
+        config,
+        features,
+      );
+
+      expect(consoleLog).toBeCalledTimes(0);
+      expect(mockedOptimizelyClient.deleteFeature).toBeCalledTimes(0);
+
+      consoleLog.mockRestore();
+    });
+
+    it('only logs when dryRun is true', async () => {
+      const consoleLog = jest.spyOn(console, 'log').mockImplementation();
+
+      const dryRun = true;
+      const mockedOptimizelyClient = {
+        deleteFeature: jest.fn(),
+      };
+      const config: OptimizelySyncConfig = {
+        someEnv: {},
+      };
+      const features: PartialFeature[] = [
+        {
+          key: 'someFeature',
+          environments: {
+            someEnv: {
+              id: 123,
+              is_primary: true,
+              rollout_rules: [
+                {
+                  audience_conditions: 'everyone',
+                  enabled: true,
+                  percentage_included: 10000,
+                },
+              ],
+            },
+          },
+        },
+      ];
+      await deleteFeatures(
+        dryRun,
+        // @ts-expect-error -- intentionally passing a partial OptimizelyConfig
+        mockedOptimizelyClient,
+        config,
+        features,
+      );
+
+      expect(consoleLog).toBeCalledTimes(1);
+      expect(mockedOptimizelyClient.deleteFeature).toBeCalledTimes(0);
+
+      consoleLog.mockRestore();
+    });
+
+    it('logs and executes dryRun is false', async () => {
+      const consoleLog = jest.spyOn(console, 'log').mockImplementation();
+
+      const dryRun = false;
+      const mockedOptimizelyClient = {
+        deleteFeature: jest.fn(),
+      };
+      const config: OptimizelySyncConfig = {
+        someEnv: {},
+      };
+      const features: PartialFeature[] = [
+        {
+          id: 123,
+          key: 'someFeature',
+          environments: {
+            someEnv: {
+              id: 456,
+              is_primary: true,
+              rollout_rules: [
+                {
+                  audience_conditions: 'everyone',
+                  enabled: true,
+                  percentage_included: 10000,
+                },
+              ],
+            },
+          },
+        },
+        {
+          id: 789,
+          key: 'otherFeature',
+          environments: {
+            someEnv: {
+              id: 987,
+              is_primary: true,
+              rollout_rules: [
+                {
+                  audience_conditions: 'everyone',
+                  enabled: true,
+                  percentage_included: 5000,
+                },
+              ],
+            },
+          },
+        },
+      ];
+      await deleteFeatures(
+        dryRun,
+        // @ts-expect-error -- intentionally passing a partial OptimizelyConfig
+        mockedOptimizelyClient,
+        config,
+        features,
+      );
+
+      expect(consoleLog).toBeCalledTimes(2);
+      // There are two features to delete so it should run twice
+      expect(mockedOptimizelyClient.deleteFeature).toBeCalledTimes(2);
+
+      consoleLog.mockRestore();
+    });
+
+    it("rejects if it is unable to find a feature's id", async () => {
+      const consoleLog = jest.spyOn(console, 'log').mockImplementation();
+
+      const dryRun = false;
+      const mockedOptimizelyClient = {
+        deleteFeature: jest.fn(),
+      };
+      const config: OptimizelySyncConfig = {
+        someEnv: {},
+      };
+      const features: PartialFeature[] = [
+        {
+          key: 'someFeature',
+        },
+      ];
+      await deleteFeatures(
+        dryRun,
+        // @ts-expect-error -- intentionally passing a partial OptimizelyConfig
+        mockedOptimizelyClient,
+        config,
+        features,
+      );
+
+      expect(consoleLog).toBeCalledTimes(2);
+      expect(consoleLog).toBeCalledWith(
+        expect.arrayContaining([
+          expect.objectContaining({
+            status: 'rejected',
+          }),
+        ]),
+      );
+      expect(mockedOptimizelyClient.deleteFeature).toBeCalledTimes(0);
+
+      consoleLog.mockRestore();
+    });
   });
 
   describe('persistFeatures', () => {
